c70aec41a71c7ccf5969c7071590920a856de42a
aamirkhan06 <aamir.campus@gmail.com>
2024-03-15 16:13:28 +0530

    pass commit 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
-public class Main {
-  
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RWOnly obj=new RWOnly();
+        //obj.name="Aamir";
+        //name has private access in com.driver.RWOnly
+        obj.setName("Aamir");
+        System.out.println(obj.getName());
+    }
 }
